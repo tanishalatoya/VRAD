facadfb571802bc2492df9eee5f901a756a0311c
Trond Makonese <trondation@gmail.com>
2020-03-24 09:46:59 -0600

    Add skelton for user login.

---
+++ src/UserLogin/UserLogin.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class UserLogin extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      reason: ''
+    };
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
