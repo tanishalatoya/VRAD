73eda17820e9d06bc23a19e05fd132cd51ec1b51
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-26 17:54:59 -0600

    Add utilize props for cards containing each area

--- src/components/AreasContainer/AreasContainer.js
+++ src/components/AreasContainer/AreasContainer.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './AreasContainer.css';
 
-const AreasContainer = () => {
-  // const allAreas = props.map(area => {
-  //   return <Area
-  //     name={area.area}
-  //     location={area.location}
-  //     about={area.about}
-  //     listings-{area.listings}
-  //     key={area.id}
-  //   />
-  // })
+const AreasContainer = (props) => {
+  const allAreas = props.listingsByArea.map(area => {
+    // console.log(area[0].areaNickname);
+    // console.log(area[0].areaDetails.name);
+    // console.log(area[0].areaDetails.location);
+    // console.log(area[0].areaDetails.about);
+    // console.log(area[0].areaDetails.id);
+    return <Area
+      key={area[0].areaDetails.id}
+      areaNickname={area[0].areaNickname}
+      name={area[0].areaDetails.name}
+      location={area[0].areaDetails.location}
+      about={area[0].areaDetails.about}
+    />
+  })
 
   return (
     <section>
-      <h1>made it</h1>
+      { allAreas }
     </section>
   )
 }
