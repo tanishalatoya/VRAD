c5c529f80716ae3c54809e8931831fe7edb95bc7
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-27 08:23:00 -0600

    Insert props to render to user profile component

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import userIcon from '../../assets/user.svg'
 import './UserProfile.css';
 
-const UserProfile = () => {
+const UserProfile = (props) => {
   return (
     <aside className='user-profile'>
-      hello!
+      <img src={userIcon} alt='user icon' />
+      <h3>{props.user.name}</h3>
+      <p>{props.user.email}</p>
+      <p>Travel Purpose</p>
+      <p>{props.user.reason}</p>
     </aside>
   )
 }
