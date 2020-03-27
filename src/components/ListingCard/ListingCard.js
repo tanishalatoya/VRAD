a3d766cec8e2b04269f772b07a53ad2e7804482d
Trond Makonese <trondation@gmail.com>
2020-03-26 20:17:32 -0600

    Create and up card for showing listing info.

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './ListingsCard.css'
+import './ListingsCard.css';
+import PropTypes from 'prop-types';
+
 
 const ListingsCard = (props) => {
   return (
     <section className='listings-card'>
       <h3>&ldquo;{props.name}&rdquo;</h3>
-      <button id={props.neighborhood_id}>View Details</button>
+      <button id={props.neighborhood_id}>View Listing Details</button>
     </section>
   )
 }
