0c5b84fc01292d8f9293101569e7b8e7827d5ece
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-26 19:33:45 -0600

    Restore function to working state

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+
+
+
 export default class UserLogin extends Component {
   constructor() {
     super();
     this.state = {
       name: '',
       email: '',
-      reason: ''
+      reason: '',
+      error: ''
     };
   }
 
-  handleChange = e => {
-    this.setState({e.target.name: e.target.value})
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
   }
 
-  loginUser = () => {
-
+  loginUser = event => {
+    event.preventDefault()
+    if (this.state.name === '' || this.state.email === ''|| this.state.reason === '') {
+      this.setState({error: 'Please Fill In All Inputs'})
+    } else {
+      const { setUserInfo } = this.props;
+      this.props.setUserInfo(this.state);
+    }
   }
 
   render() {
     return (
       <form>
+      <h1>Welcome to V-RAD</h1>
+      <div>
         <input
           type='text'
           placeholder='name'
@@ -33,10 +45,13 @@ export default class UserLogin extends Component {
           type='text'
           placeholder='email'
           value={this.state.email}
-          name='eamil'
+          name='email'
           onChange={this.handleChange}
         />
+      </div>
+      <div>
         <label htmlFor="reason-for-login">Reason for visit</label>
+      </div>
         <select onChange={this.handleChange} name='reason'>
           <option disabled selected value>
           -- select an option --
@@ -45,8 +60,17 @@ export default class UserLogin extends Component {
           <option value="vacation">Vacation</option>
           <option value="other">Other</option>
         </select>
-        <button onClick={this.loginUser}>Login</button>
+        <div>
+          <Link onClick={() => this.loginUser} className="login-button" to="/areas">
+            Login
+          </Link>
+        </div>
+      {this.state.error &&
+        <div>
+          <p>{this.state.error}</p>
+        </div>
+      }
       </form>
-    )
+    );
   }
 }
