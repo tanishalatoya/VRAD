11e3432531442f7bc06b3a08c8cb6a2524d88cc8
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-26 19:40:45 -0600

    Create functional component to render user's profile

---
+++ src/components/UserProfile/UserProfile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './UserProfile.css';
+
+const UserProfile = () => {
+  return (
+    <aside className='user-profile'>
+      hello!
+    </aside>
+  )
+}
+
+
+export default UserProfile;
