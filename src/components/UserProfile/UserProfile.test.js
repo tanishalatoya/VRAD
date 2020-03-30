7890f712cbbd39d708fba55c3411fcbdaba3896b
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-29 18:13:31 -0600

    Test that a user profile component renders with a defined user

--- src/components/UserProfile/UserProfile.test.js
+++ src/components/UserProfile/UserProfile.test.js
@@ -1,8 +1,16 @@
 import React from 'React';
-import { render } from '@testing-library/react';
+import UserProfile from './UserProfile';
+import { BrowserRouter } from 'react-router-dom';
+import { render, debug } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 describe('UserProfile', () => {
   it('should render a user\'s profile to the page', ()=> {
+    const { queryByText, findByText } = render(<BrowserRouter>
+        <UserProfile user={'Willy Wonka'}/>
+      </BrowserRouter>)
 
+      const userName = findByText('Willy Wonka')
+      expect(userName).toBeDefined();
   });
 });
