8ca2a981ce863be2782f5bb7ec824da09e21193a
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-30 07:36:40 -0600

    Change functional component to class component

--- src/components/Areas/Areas.js
+++ src/components/Areas/Areas.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './Areas.css';
 
-const Areas = (props) => {
-  return (
-    <section className='area-card'>
-      <h3>&ldquo;{props.areaNickname}&rdquo;</h3>
-      <h4>{props.name}</h4>
-      <p>{props.location}</p>
-      <p>{props.about}</p>
-      <Link to='/listings'>
-      <button id={props.areaDetails.id}>View Listings</button>
-      </Link>
-    </section>
-  )
-}
+export default class Areas extends Component {
+  constructor() {
+    super();
+    this.state = {
+      areaId: null
+    }
+  }
 
-export default Areas;
+  render() {
+    return (
+      <section className='area-card'>
+        <h3>&ldquo;{this.props.areaNickname}&rdquo;</h3>
+        <h4>{this.props.name}</h4>
+        <p>{this.props.location}</p>
+        <p>{this.props.about}</p>
+        <Link to='/listings'>
+        <button id={this.props.id}>View Listings</button>
+        </Link>
+      </section>
+    )
+  }
+
+}
 
 Areas.propTypes = {
   key: PropTypes.number,
