474d379f295f9e9f3332dd93d9a20914617eb5f2
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-30 17:28:46 -0600

    Add grid styling to container to feature user profile on the left

--- src/components/ListingDetails/ListingDetails.js
+++ src/components/ListingDetails/ListingDetails.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import UserProfile from '../../components/UserProfile/UserProfile.js';
+import './ListingDetails.css';
 
 const ListingDetails = (props) => {
   return (
     <section className='listing-details-container'>
       <UserProfile user={props.user}/>
-      <h1>{props.currentListing.listingName}</h1>
+      <section className='listing-details'>
+        <h1>{props.currentListing.listingName}</h1>
+      </section>
     </section>
   )
 }
