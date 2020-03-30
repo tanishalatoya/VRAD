93f637ac613f1c0f7e5173f2c0f80dd977209c53
Trond Makonese <trondation@gmail.com>
2020-03-30 16:18:14 -0600

    Listing details now render, need to change to functional component.

--- src/components/ListingDetails/ListingDetails.js
+++ src/components/ListingDetails/ListingDetails.js
@@ -1,54 +1,17 @@
 import React, { Component } from 'react';
-import './ListingDetail.scss';
 
-class ListingDetail extends Component {
+class ListingDetails extends Component {
   constructor() {
     super();
-    this.state = {
-      currentListing: undefined
-    };
   }
 
-  fetchListings() {
-    fetch('http://localhost:3001/api/v1/areas')
-    .then(response => response.json())
-    .then(areasData => {
-      const allData = areasData.areas.map(area => {
-        return fetch(`http://localhost:3001${area.details}`)
-        .then(res => res.json())
-        .then(details => {
-          const allListings = details.listings.map(listing => {
-            return fetch(`http://localhost:3001${listing}`)
-            .then(res => res.json())
-          })
-          return Promise.all(allListings)
-        })
-      })
-      return Promise.all(allData)
-    })
-    .then(err => console.log(err))
+  render() {
+    console.log('made-it')
+    return (
+      <section>
+        <h1>{this.props.currentListing.listingName}</h1>
+      </section>
+    )
   }
-
-  fetchListingDetails(id){
-    fetchListings()
-    return fetch(`http://localhost:3001/api/v1/listings/${id}`)
-    .then(res => res.json())
-    .catch(err => console.log(err))
-  }
-
-  componentDidMount() {
-    const {
-      match: {
-        params: { listing_id }
-      }
-    } = this.props;
-    fetchListingDetails(listing_id)
-    .then(currentListing => this.setState({ currentListing }));
-  }
-
-  render() {    
-  }
-
 }
-
-export default ListingDetail;
+export default ListingDetails;
