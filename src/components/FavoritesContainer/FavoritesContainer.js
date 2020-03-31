b6ddda203ea9bac8cb532b9042e44a6bc0328205
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-31 12:54:54 -0600

    Fix merge conflicts

---
+++ src/components/FavoritesContainer/FavoritesContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import  Favorites from '../Favorites/Favorites';
+import UserProfile from '../../components/UserProfile/UserProfile.js';
+import PropTypes from 'prop-types';
+import './FavoritesContainer.css';
+
+export const FavoritesContainer = (props) => {
+
+
+  return (
+    <section className='favorites-container'>
+      <UserProfile user={props.user}/>
+      <section className='favorite-cards-container'>
+        <p>Favorites here.</p>
+      </section>
+    </section>
+  )
+}
--- src/components/UserProfile/UserProfile.test.js
+++ src/components/UserProfile/UserProfile.test.js
@@ -1,8 +1,26 @@
-import React from 'React';
-import { render } from '@testing-library/react';
+import React from 'react';
+import UserProfile from './UserProfile';
+import { BrowserRouter } from 'react-router-dom';
+import { render, debug, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 describe('UserProfile', () => {
-  it('should render a user\'s profile to the page', ()=> {
+  it('should render a user\'s profile to the page', async ()=> {
 
+    const user = {
+      name: 'Willy',
+      email: 'hello@world.com'
+      reason: 'Vacation'
+    }
+    const { queryByText, findByText } = render(<BrowserRouter>
+        <UserProfile
+          name={'Willy Wonka'}
+          email={'willyw@wonkafactory.com'}
+          reason={'Vacation'}
+        />
+      </BrowserRouter>)
+
+      const userName = await waitFor(() => getByText('Vacation'))
+      expect(userName).toBeInTheDocument();
   });
 });
