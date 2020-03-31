57684fe9d012df04015e17d2d12b84c6937ed213
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-31 14:43:52 -0600

    Will not be merging this branch for the project deadline, but saving to continue working on this later

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ListingCard.css';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-export const ListingCard = (props) => {
-  return (
-    <section className='listings-card'>
-      <h2>&ldquo;{props.listingName}&rdquo;</h2>
-      <p>Street: {props.listingAddress1}</p>
-      <p>Zip Code: {props.listingAddress2}</p>
-      <div>
-      <button>Favorite</button>
-      <Link to={`/areas/${props.areaId}/listing/${props.id}/details`}>
-      <button id={props.id}>View Listing Details</button>
-      </Link>
-      </div>
-    </section>
-  )
+export class ListingCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedFavorites: []
+    }
+    this.favoritesHolding = []
+  }
+
+  updateFavoriteStatus(e) {
+    if (!this.favoritesHolding.includes(e.target.id)) {
+      this.favoritesHolding.push((e.target.id))
+      this.setState({ selectedFavorites: this.favoritesHolding}, () => console.log('in state', this.state.selectedFavorites))
+    } else {
+      this.favoritesHolding.splice(this.favoritesHolding.indexOf(e.target.id), 1)
+      this.setState({ selectedFavorites: this.favoritesHolding}, () => console.log('in strate', this.state.selectedFavorites))
+    }
+  }
+
+  render() {
+    return (
+      <section className='listings-card'>
+        <h2>&ldquo;{this.props.listingName}&rdquo;</h2>
+        <p>Street: {this.props.listingAddress1}</p>
+        <p>Zip Code: {this.props.listingAddress2}</p>
+        <div>
+        <button id={this.props.id} onClick={e => this.updateFavoriteStatus(e)}>Favorite</button>
+        <Link to={`/areas/${this.props.areaId}/listing/${this.props.id}/details`}>
+        <button>View Listing Details</button>
+        </Link>
+        </div>
+      </section>
+    )
+  }
 }
 
 // export default ListingsCard;
