fd4a8c3b1c84b504b20b2ce409888bead6b3b7e5
Trond Makonese <trondation@gmail.com>
2020-03-31 12:17:01 -0600

    Created test for Listing Card, Login form, need more info in Listing container.

--- src/components/ListingCard/ListingCard.test.js
+++ src/components/ListingCard/ListingCard.test.js
@@ -1,8 +1,24 @@
 import React from 'React';
-import { render } from '@testing-library/react';
+import ListingCard from './ListingCard.js';
+import { BrowserRouter } from 'react-router-dom';
+import { render, debug } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-describe('Listimg Card', () => {
-  it('should render listing cards to the page', ()=> {
+describe('Listings', () => {
+  it('should render listing cards to the page with the correct information', ()=> {
+    const { getByText, debug } = render(<BrowserRouter>
+      <ListingCard
+        ListingName={'Hip RiNo Party Spot'}
+        listingAddress1={'2250 Lawrence St'}
+        listingAddress2={'80205'}
+      /></BrowserRouter>)
 
+    const name = getByText('Hip RiNo Party Spot');
+    const address1 = getByText('Street: 2250 Lawrence St');
+    const address2 = getByText('80205')
+
+    expect(name).toBeInTheDocument();
+    expect(address1).toBeInTheDocument();
+    expect(address2).toBeInTheDocument();
   });
 });
